ac910a65f75a1e291ef9e23cd4359b197e534ca8
vivalafreak1 <arieftaufikrahman123@gmail.com>
2024-03-05 10:31:57 +0700

    first commit

---
+++ src/states/isPreload/action.js
@@ -0,0 +1,40 @@
+import { hideLoading, showLoading } from 'react-redux-loading-bar';
+import api from '../../utils/api';
+import { setAuthUserActionCreator } from '../authUser/action';
+
+const ActionType = {
+  SET_IS_PRELOAD: 'SET_IS_PRELOAD',
+};
+
+function setIsPreloadActionCreator(isPreload) {
+  return {
+    type: ActionType.SET_IS_PRELOAD,
+    payload: {
+      isPreload,
+    },
+  };
+}
+
+function asyncPreloadProcess() {
+  return async (dispatch) => {
+    dispatch(showLoading());
+    try {
+      // Preload proccess
+      const authUser = await api.getOwnProfile();
+      dispatch(setAuthUserActionCreator(authUser));
+    } catch (error) {
+      // Fallback process
+      dispatch(setAuthUserActionCreator(null));
+    } finally {
+      // End preload process
+      dispatch(setIsPreloadActionCreator(false));
+    }
+    dispatch(hideLoading());
+  };
+}
+
+export {
+  ActionType,
+  setIsPreloadActionCreator,
+  asyncPreloadProcess,
+};
